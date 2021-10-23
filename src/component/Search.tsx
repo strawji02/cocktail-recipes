c811322a8160de99c075a9398430835e15e54e03
황준혁 <strawji02@naver.com>
2021-10-23 23:41:26 +0900

    search bar

--- src/component/Search.tsx
+++ src/component/Search.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styled from "styled-components";
+
+const SearchWindow = styled.input``;
 
 const Search = () => {
-  return <div>검색해보세요!</div>;
+  return (
+    <div>
+      <SearchWindow placeholder="아무거나 검색해보세요!"></SearchWindow>
+    </div>
+  );
 };
 
 export default Search;
