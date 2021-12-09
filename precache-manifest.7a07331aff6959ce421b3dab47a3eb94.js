a29cbbfaf5082a4529dd60dc0cce5f92342a96f8
황준혁 <strawji02@naver.com>
2021-12-10 00:26:46 +0900

    Updates

---
+++ precache-manifest.7a07331aff6959ce421b3dab47a3eb94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496b9a37de3880f1bcd450fffd6bb0bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a984739879b10fce2a5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5bc1dfe39b252fac8646",
+    "url": "/static/js/2.aeec9074.chunk.js"
+  },
+  {
+    "revision": "fef9ff8338562dee005c72e4f95acffe",
+    "url": "/static/js/2.aeec9074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a304396c9480f092d725",
+    "url": "/static/js/3.2b9cd248.chunk.js"
+  },
+  {
+    "revision": "4a984739879b10fce2a5",
+    "url": "/static/js/main.389642b3.chunk.js"
+  },
+  {
+    "revision": "ee2ea7305566e0c0a890",
+    "url": "/static/js/runtime-main.6a7f2ee2.js"
+  },
+  {
+    "revision": "3d7c8dfd2c328c725c5f387f9b0e2b6a",
+    "url": "/static/media/cocktail.3d7c8dfd.jpg"
+  }
+]);
